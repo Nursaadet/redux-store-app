c7ec8d82366d37d0aaa1abc31fcba0a3b4f10375
Nursaadet <nursaadetozdemir@gmail.com>
2025-10-08 00:07:13 +0300

    Add loading, error, and product list rendering to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,27 @@
+import React, { useEffect, useState } from "react";
 import SearchComp from "../components/SearchComp";
-import { useState, useEffect } from "react";
 import axios from "axios";
-import { useAppDispatch } from "../app/hooks";
-import { fetchFail, fetchStart, getSuccessProduct } from "../features/productsSlice";
-import { Products } from "../models/models";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import {
+  fetchFail,
+  fetchStart,
+  getSuccessProduct,
+} from "../features/productsSlice";
+import { EventFunc, Products } from "../models/models";
+
 
 const Home = () => {
   const [search, setSearch] = useState<string>("");
   const dispatch = useAppDispatch();
+  const { loading, error, productsList } = useAppSelector(
+    (state) => state.products
+  );
+
 
   const getData = async () => {
     dispatch(fetchStart());
     try {
+      
       const { data } = await axios.get<Products>(
         `https://dummyjson.com/products/search?q=${search}`
       );
@@ -22,18 +32,35 @@ const Home = () => {
       dispatch(fetchFail());
     }
   };
-
+  console.log(error);
   useEffect(() => {
     getData();
   }, [search]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  
+  const handleChange: EventFunc = (e) => {
     setSearch(e.target.value);
   };
+
 
   return (
     <div>
       <SearchComp handleChange={handleChange} />
+      {loading ? (
+        <div className="mt-52">
+          <p className="text-center text-red-600">Products loading...</p>
+        </div>
+      ) : error ? (
+        <div className="mt-52">
+          <p className="text-center text-red-600">Something went wrong...</p>
+        </div>
+      ) : (
+        <div>
+          {productsList.map((item) => (
+            <p>{item.title}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
