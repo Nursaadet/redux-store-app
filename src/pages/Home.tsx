7ac2073d62a754a8152a0727efb93d54e74adb22
Nursaadet <nursaadetozdemir@gmail.com>
2025-10-07 23:46:56 +0300

    Implement search input and dynamic product fetching

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import SearchComp from "../components/SearchComp";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useAppDispatch } from "../app/hooks";
+import { fetchFail, fetchStart, getSuccessProduct } from "../features/productsSlice";
+import { Products } from "../models/models";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [search, setSearch] = useState<string>("");
+  const dispatch = useAppDispatch();
 
-export default Home
+  const getData = async () => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.get<Products>(
+        `https://dummyjson.com/products/search?q=${search}`
+      );
+      console.log(data.products);
+      dispatch(getSuccessProduct(data.products));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [search]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <div>
+      <SearchComp handleChange={handleChange} />
+    </div>
+  );
+};
+
+export default Home;
